897683d26ec5edac42ba65e8467bf62d485b911f
ChakritMesup <chakritmesupphaisan@gmail.com>
2024-11-13 10:43:16 +0700

    login template

---
+++ football-web/src/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const LoginSignup = () => {
+    return (
+        <div>
+            <div>
+                <div>Email</div>
+                <div><input type="email" /></div>
+            </div>
+            <div>
+                <div>Password</div>
+                <div><input type="password" /></div>
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </div>
+    )
+}
+
+export default LoginSignup
