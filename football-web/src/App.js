79ef9e1e8ad7d5fea31768c3b813baee72ce9b4a
Jakkrin Prasithisang <jakkrin2765@hotmail.com>
2024-12-01 21:26:07 +0700

    remove trash

--- football-web/src/App.js
+++ football-web/src/App.js
@@ -4,8 +4,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './Component/Home/HomePage';
 import LoginSignup from './Component/LoginSignup/LoginSignup';
 import NavBar from './Component/navbar';
-import ForumPage from './Component/forumpage';
-import ThreadPage from './Component/threadpage';
 import PlayerListing from "./Component/Playerlisting/Playerlisting";
 import PlayerDetail from "./Component/Playerdetail/Playedetail";
 import SentOffers from './Component/Sentoffer/SentOffers';
@@ -20,18 +18,14 @@ function App() {
     <Router> {/* Wrap your Routes with Router */}
     <NavBar />
       <Routes>
-        <Route path="/" element={<ForumPage threads={threads} setThreads={setThreads}/>} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/login" element={<LoginSignup action="Login"/>} />
         <Route path="/signup" element={<LoginSignup action="Sign Up"/>} />
         <Route path="/players/:email" element={<PlayerDetail />} />
         <Route path="/offers/sent" element={<SentOffers />} />
         <Route path="/players/:email/view-offers" element={<ViewOffers />} />
-        <Route
-          path="/thread/:threadId"
-          element={<ThreadPage threads={threads}/>}
-        />
-         <Route path="/players" element={<PlayerListing />} />
+        <Route path="/players" element={<PlayerListing />} />
       </Routes>
     </Router>
   );
